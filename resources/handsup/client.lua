ab6bc8fc2a0a6859a28f134313be3a84ac63d7fd
Benjamin Robinet <benjaminrobinet@users.noreply.github.com>
2017-05-23 18:58:00 +0200

    Update hands up

--- resources/handsup/client.lua
+++ resources/handsup/client.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		if IsControlPressed(1, 323) then --Start holding X
-			TaskHandsUp(GetPlayerPed(-1), 1000, GetPlayerPed(-1), -1, true) -- Perform animation.
+			TaskHandsUp(GetPlayerPed(-1), 1000, -1, -1, true) -- Perform animation.
 		end
 	end
 end)
